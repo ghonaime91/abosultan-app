c0ade69788203317ebdf16fa2227c9c9e01f317e
ahmed <hozn_abady@gmail.com>
2025-01-06 11:13:49 +0200

    modify index blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
 </head>
 <body>
-    <h1>Working ......</h1>
+    <h1 class="text-center mt-5">Working ...</h1>
 </body>
 </html>
