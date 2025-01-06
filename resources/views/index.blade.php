0a148c555ddf7ff6123527a558036efd90aa461b
ahmed <hozn_abady@gmail.com>
2025-01-06 11:11:31 +0200

    modify index blade

---
+++ resources/views/index.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Wait For Us</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <h1>Working ......</h1>
+</body>
+</html>
