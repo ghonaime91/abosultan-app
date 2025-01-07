92ed6b0ee4d24f8fc9a55723630991405fa3abfe
ahmed <hozn_abady@gmail.com>
2025-01-07 05:25:13 +0200

    modify email verification success

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,13 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Auth\Events\Verified;
 
 /**
  * Auth routes start
@@ -10,20 +16,35 @@ use App\Http\Controllers\AuthController;
 # Register a new user route
 Route::post('/register', [AuthController::class, 'register'])
     ->name('register');
-    
-# Verify email routes
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/email/verify', [AuthController::class, 'showVerificationNotice'])
-        ->name('verification.notice');
 
-    Route::post('/email/verification-notification', [AuthController::class, 'sendVerificationNotification'])
-        ->middleware(['throttle:6,1'])
-        ->name('verification.send');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/email/verify', function () {
+        return response()->json(['message' => 'يرجى التحقق من بريدك الإلكتروني.']);
+    })->name('verification.notice');
+
+    Route::post('/email/verification-notification', function (Request $request) {
+        $request->user()->sendEmailVerificationNotification();
+
+        return response()->json(['message' => 'تم إرسال رابط التحقق!']);
+    })->middleware(['throttle:6,1'])->name('verification.send');
 });
 
-Route::get('/email/verify/{id}/{hash}', [AuthController::class, 'verifyEmail'])
-    ->middleware(['signed'])
-    ->name('verification.verify');
+
+Route::get('/email/verify/{id}/{hash}', function (Request $request) {
+    $user = User::findOrFail($request->id);
+
+    if ($user->email_verified_at) {
+        return response()->json(['message' => 'البريد الإلكتروني مفعل بالفعل.']);
+    }
+
+    if ($user->markEmailAsVerified()) {
+        event(new Verified($user));
+        return response()->json(['message' => 'تم التحقق من بريدك الإلكتروني.']);
+    }
+
+    return response()->json(['message' => 'خطاء في التحقق من بريدك الإلكتروني.']);
+    
+})->middleware(['signed'])->name('verification.verify');
 
 # Login route
 Route::post('/login', [AuthController::class, 'login'])
